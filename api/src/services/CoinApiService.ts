4008027a9f318bcba07f4da9f788f390fcfecefc
Romel Lauron <rlauron@cyrious.com>
2018-07-12 03:57:46 +0800

    added api

--- api/src/services/CoinApiService.ts
+++ api/src/services/CoinApiService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 export class CoinApiService {
 
     private baseUrl: string = 'https://rest.coinapi.io';
-    private apiKey: string = '';
+    private apiKey: string = 'FA49C145-2663-48E9-BFF7-FF085C4F1CE9';
     private axios = axios;
 
    private get axiosInstance() {
